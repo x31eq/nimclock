ced4aaf1f7ac15a683a75ae12034ea26e2e023f7
Graham Breed <gbreed@gmail.com>
2020-04-28 20:57:27 +0100

    festamp for the current time

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: fedate feedate
+all: fedate feedate festamp
 
 feedate: feedate.nim
 	nim c -d:release feedate.nim
 
 fedate: fedate.nim
 	nim c -d:release fedate.nim
+
+festamp: festamp.nim
+	nim c -d:release festamp.nim
---
+++ festamp.nim
@@ -0,0 +1,25 @@
+import strutils, times
+
+let instant = getLocalTime(getTime())
+
+let month = Natural(instant.month)
+let quarter = instant.year * 4 + month div 3
+var qday = month mod 3 * 38
+if month == 2 or month == 11:
+    qday -= 1
+let weekday = (Natural(instant.weekday) + 1) mod 7
+qday += instant.monthday + 5 - weekday
+var sec = instant.second
+var tick = sec div 15 - sec div 60
+sec -= tick * 15
+tick += instant.minute * 4
+let week = qday div 7
+let halfday = weekday * 2 + Natural(instant.hour > 11)
+let hour = instant.hour mod 12
+tick = tick * 16 div 15
+
+proc lowerHex(x: BiggestInt, len: Positive): string =
+     strutils.toLowerAscii(strutils.toHex(x, len))
+
+echo lowerHex(quarter mod 0x1000, 3), lowerHex(week, 1), '.',
+     lowerHex(halfday, 1), lowerHex(hour, 1), lowerHex(tick, 2)
