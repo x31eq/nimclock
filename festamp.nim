106fa676a9c1b74d376ad0467c71444894cde666
Graham Breed <gbreed@gmail.com>
2020-04-30 18:56:25 +0100

    Removed unused line from festamp

--- festamp.nim
+++ festamp.nim
@@ -17,7 +17,6 @@ let weekday = (Natural(instant.weekday) + 1) mod 7
 qday += instant.monthday + 5 - weekday
 var sec = instant.second
 var tick = sec div 15 - sec div 60
-sec -= tick * 15
 tick += instant.minute * 4
 let week = qday div 7
 let halfday = weekday * 2 + Natural(instant.hour > 11)
